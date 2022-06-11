381d796a45c69878ae272d53d6fef5ff9f4fa2dc
Rafał <riderbrand@gmail.com>
2022-06-11 23:57:33 +0200

    refactor: move js from index.html to main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
-const app = document.getElementById('root');
+window.onload = () => {
+  const section = document.getElementById('modalSection');
+
+  const modalContent = `Hi, welcome in this precious modal and have a nice day :)`;
+
+  let modal = new Modal('Alert!', modalContent);
+  section.appendChild(modal.getModal());
+};
